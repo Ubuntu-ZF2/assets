6d281308da0a9858dace4ada3b8745a99d896d74
Sergio Schvezov <sergio.schvezov@canonical.com>
2013-04-22 16:44:17 -0300

    Adding stamp copy to this repo and changing stamp filename
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- UbuntuAssets.mk
+++ UbuntuAssets.mk
@@ -5,9 +5,12 @@
 #
 #
 
-LOCAL_PATH:= frameworks/base/data/sounds
 UBUNTU_PATH:= ubuntu/assets
 
 # Effects
 PRODUCT_COPY_FILES += \
 	$(UBUNTU_PATH)/camera_click.ogg:system/media/audio/ui/camera_click.ogg
+
+# Build stamp
+PRODUCT_COPY_FILES += \
+    ubuntu/assets/ubuntu_stamp:/system/ubuntu_stamp
