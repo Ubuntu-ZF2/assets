586127f3882d22c3f808f3a05364cd89570890df
Sergio Schvezov <sergio.schvezov@canonical.com>
2014-02-10 16:33:41 -0300

    Making assets not depend on product.
    Assets are now installed independent of product to get rid of the cm
    dependency using a proper Android.mk file instead of being called
    from the cm vendor tree which we don't have on AOSP.
    
    Only the autopilot-finger.idc file is included in this change.
    
    Change-Id: I75e007ed095d10558cca01343a3e0571c8bf19fe
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Testing assets
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := autopilot-finger.idc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SYSTEM
+LOCAL_MODULE_PATH := $(TARGET_OUT)/usr/idc
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
